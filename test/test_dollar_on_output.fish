d3bbcd7fe952b962363580c6cf72ec7ef2725ad8
Eddie Cao <edc@users.noreply.github.com>
2020-10-21 15:42:10 -0700

    fix test_dollar_on_output
    Fixes #90

--- test/test_dollar_on_output.fish
+++ test/test_dollar_on_output.fish
@@ -1,4 +1,4 @@
-set root (dirname (dirname (status -f)))
+set root (dirname (status -f))/..
 source $root/functions/bass.fish
 
 bass source $root/test/fixtures/dollar_output.sh | grep -q 'some program output with the $ symbol in it'
