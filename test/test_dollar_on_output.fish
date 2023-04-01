1077f7d29bf0616de590ec6edb99fa13588eb507
Michael Jarvis <michael@jarvis.com>
2023-04-01 13:07:08 -0500

    add color to tests

--- test/test_dollar_on_output.fish
+++ test/test_dollar_on_output.fish
@@ -3,4 +3,4 @@ source $root/functions/bass.fish
 
 bass source $root/test/fixtures/dollar_output.sh | grep -q 'some program output with the $ symbol in it'
 
-echo 'Success'
+echo (set_color green)Success(set_color normal)
--- test/test_non_zero_returncode.fish
+++ test/test_non_zero_returncode.fish
@@ -4,8 +4,8 @@ source $root/functions/bass.fish
 bass false
 
 if test $status -ne 1
-	echo 'failed: bass exited with status' $status 'when 1 is expected'
+	echo (set_color red)failed: bass exited with status $status when 1 is expected(set_color normal)
 	exit 1
 end
 
-echo 'Success'
+echo (set_color green)Success(set_color normal)
